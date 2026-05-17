07920d66657b18cb334da6de585a515b399c4e59
haiquanng <nguyenhaiquan.data@gmail.com>
2026-05-17 18:22:43 +0700

    fix(deploy): get (/) and docker base cpu to descrease size image

--- app/routers/health.py
+++ app/routers/health.py
@@ -1,6 +1,10 @@
 from fastapi import APIRouter
 router = APIRouter()
 
+@router.get("/")
+async def root():
+    return {"status": "ok"}
+
 @router.get("/health")
 async def health():
     return {"status": "ok", "service": "tutora response", "version": "1.0.0"}
